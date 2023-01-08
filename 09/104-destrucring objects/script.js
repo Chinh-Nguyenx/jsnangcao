407f13ca39856b78c8b88516ae70816cab73a81d
Chinh-Nguyenx <chinhndph25585@fpt.edu.vn>
2023-01-09 03:10:14 +0700

    destrucringObjects

---
+++ 09/104-destrucring objects/script.js
@@ -0,0 +1,59 @@
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23. Firenze, Italy',
+    categories: ['Italian','Pizzeria','Vegetarian','Organic'],
+    starterMenu: ['Focaccia','Bruschetta','Garlic Bread','Caprese Salad'],
+    mainMenu: ['Pizza','Pasta','Risotto'],
+
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0,
+            close: 24,
+        },
+    },
+
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+    orderDelivery: function ({
+        starterIndex=1,
+        mainIndex=0,
+        time='20:00',
+        address,}) {
+        console.log('Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]]} will be delivered to ${address} at ${time}'
+        );
+    }
+};
+
+restaurant.orderDelivery({
+    time: '22:30',
+    address: 'Via del Sloe, 21',
+    mainIndex: 2,
+    starterIndex: 2,
+});
+
+const {name, openingHours, categories} = restaurant;
+console.log(name,openingHours,categories);
+
+const {name: restaurantName, openingHours: hours, categories: tags} = restaurant;
+console.log(restaurantName,hours,tags);
+
+const {menu = [], starterMenu: starter = []}=restaurant;
+console.log(menu,starter);
+
+let a =11;
+let b = 99;
+const obj = {a:23,b:7,c:14};
+({a,b} = obj);
+console.log(a,b);
+
+const {fri: {open, close}} = openingHours;
+console.log(open,close);
